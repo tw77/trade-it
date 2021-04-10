3d10392c7c8333a919e4658fc3c84ededb1e5595
Thomas Weideman <thomas.weideman@gmail.com>
2021-04-10 21:09:22 +0000

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,38 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'listings#index'
+
   namespace :api do # /api/data
 
     get '/data', to: 'tests#index'
     
-    resources :dogs
+    resources :listings
+
+    resources :users do
+        resource :profile
+        resource :wishlist
+        resource :add_listing
+        resources :proposals do 
+          resource :accepted_proposal, only: [:show]
+        end
+        resource :suggestions
+    end
+
+
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+
+    get '/signup' => 'users#new'
+
+    post '/users' => 'users#create'
 
   end
+ 
+
+
 
   get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
