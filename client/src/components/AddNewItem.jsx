7086b00a2c6a3737589c3944442c0a1c62f3cf26
Thomas Weideman <44182629+tw77@users.noreply.github.com>
2021-04-18 20:56:51 -0700

    Styling, switch price range to category (#29)

--- client/src/components/AddNewItem.jsx
+++ client/src/components/AddNewItem.jsx
@@ -1,146 +1,169 @@
-import React, { useReducer, useState } from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import MenuItem from '@material-ui/core/MenuItem';
-
+import React, { useReducer, useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Container,
+  MenuItem,
+} from "@material-ui/core";
+const BootstrapButton2 = withStyles({
+  root: {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "4px 8px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: "#2a9d8f",
+    borderColor: "#2a9d8f",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#006F3C",
+      borderColor: "#006F3C",
+      boxShadow: "none",
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#006F3C",
+      borderColor: "#006F3C",
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    },
+  },
+})(Button);
 const categories = [
   {
     value: 1,
-    label: 'Electronics',
+    label: "Electronics",
   },
   {
     value: 2,
-    label: 'Clothing',
+    label: "Clothing",
   },
   {
     value: 3,
-    label: 'Books',
+    label: "Books",
   },
   {
     value: 4,
-    label: 'Furniture',
+    label: "Furniture",
   },
   {
     value: 5,
-    label: 'Appliances',
+    label: "Appliances",
   },
   {
     value: 6,
-    label: 'Cars',
+    label: "Cars",
   },
   {
     value: 7,
-    label: 'Bicycles',
+    label: "Bicycles",
   },
   {
     value: 8,
-    label: 'Carpets',
+    label: "Carpets",
   },
   {
     value: 9,
-    label: 'Plants',
+    label: "Plants",
   },
   {
     value: 10,
-    label: 'Instruments',
+    label: "Instruments",
   },
 ];
-
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
     paddingTop: "50px",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
-
 const formReducer = (state, event) => {
-  if(event.reset) {
+  if (event.reset) {
     return {
-      name: '',
-      description: '',
-      price_range: '',
-      picture: ''
-    }
+      name: "",
+      description: "",
+      category: "",
+      picture: "",
+    };
   }
   return {
     ...state,
-    [event.name]: event.value
-  }
- }
-
- const mockListing = {
-  "id": 1,
-  "name": "Linen shirt, M",
-  "description": "Rarely-worn light blue linen shirt, very comfortable",
-  "picture": "https://images.unsplash.com/photo-1598961942613-ba897716405b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1",
-  "owner_id": 3,
-  "storer_id": 3,
-  "category_id": 2
-}
-
+    [event.name]: event.value,
+  };
+};
+const mockListing = {
+  id: 1,
+  name: "Linen shirt, M",
+  description: "Rarely-worn light blue linen shirt, very comfortable",
+  picture:
+    "https://images.unsplash.com/photo-1598961942613-ba897716405b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1",
+  owner_id: 3,
+  storer_id: 3,
+  category_id: 2,
+};
 export default function AddNewItem(props) {
   // const history = useHistory();
-
   // console.log('listings in AddNewItem.jsx: ', listings);
-
   const classes = useStyles();
   const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
-
-  const handleChange = event => {
+  const handleChange = (event) => {
     setFormData({
       name: event.target.name,
       value: event.target.value,
     });
-  }
-
-  console.log('props.listings in AddNewItem', props.listings);
-
+  };
+  console.log("props.listings in AddNewItem", props.listings);
   const handleSubmit = (event) => {
     event.preventDefault();
     props.publishListing(formData);
     setSubmitting(true);
-    console.log('new item: ', formData);
-
+    console.log("new item: ", formData);
     setTimeout(() => {
       setSubmitting(false);
       setFormData({
-        reset: true
-      })
+        reset: true,
+      });
     }, 3000);
-  }
-
-
- // axios.post(`/api/listings`)
-
-
-
+  };
+  // axios.post(`/api/listings`)
   // back naviagation with a "Cancel" button?
-
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Add Item
+        <Typography variant="h5" align="left" color="textPrimary">
+          Add new item
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -154,7 +177,7 @@ export default function AddNewItem(props) {
             autoComplete="name"
             autoFocus
             onChange={handleChange}
-            value={formData.name || ''}
+            value={formData.name || ""}
           />
           <TextField
             id="outlined-multiline-static"
@@ -166,7 +189,7 @@ export default function AddNewItem(props) {
             name="description"
             variant="outlined"
             onChange={handleChange}
-            value={formData.description || ''}
+            value={formData.description || ""}
           />
           <TextField
             variant="outlined"
@@ -175,12 +198,14 @@ export default function AddNewItem(props) {
             fullWidth
             margin="normal"
             name="category"
-            select onChange={handleChange} >
+            select
+            onChange={handleChange}
+          >
             {categories.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))} 
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
           </TextField>
           <TextField
             variant="outlined"
@@ -193,28 +218,35 @@ export default function AddNewItem(props) {
             id="picture"
             autoComplete="picture"
             onChange={handleChange}
-            value={formData.picture || ''}
+            value={formData.picture || ""}
           />
-          
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            className={classes.submit}
-          >
-            Add New Item
-          </Button>
-          {submitting &&
-       <div>
-         You are posting the following listing:
-         <ul>
-           {Object.entries(formData).map(([name, value]) => (
-             <li key={name}><strong>{name}</strong>: {value.toString()}</li>
-           ))}
-         </ul>
-       </div>
-      }
+          <Grid container spacing={0} direction="row" justify="flex-end">
+            <Grid item xs={4}>
+              <BootstrapButton2
+                type="submit"
+                style={{ marginTop: "8px" }}
+                variant="contained"
+                color="primary"
+                disableRipple
+                className={classes.margin}
+                onClick={handleSubmit}
+              >
+                Add Item
+              </BootstrapButton2>
+            </Grid>
+          </Grid>
+          {submitting && (
+            <div>
+              You are posting the following listing:
+              <ul>
+                {Object.entries(formData).map(([name, value]) => (
+                  <li key={name}>
+                    <strong>{name}</strong>: {value.toString()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </form>
       </div>
     </Container>
