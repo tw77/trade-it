749646e27d549c18ec1af3fa8c3d2f9bd41d38ab
Thomas Weideman <44182629+tw77@users.noreply.github.com>
2021-05-08 19:10:40 -0700

    Refactor selectors.js (#48)
    * Revise findMostRepresented for consistency
    
    * Shorten code in getTradesProposedToUser
    
    * 51, 64 in selectors: params on same line

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -1,60 +1,51 @@
 export function findMostRepresented(userWishCategories) {
   const frequency = {};
-  userWishCategories.forEach(function (id) {
+  userWishCategories.forEach((id) => {
     frequency[id] = 0;
   });
-  const uniques = userWishCategories.filter(function (id) {
+  const uniques = userWishCategories.filter((id) => {
     return ++frequency[id] === 1;
   });
-  return uniques.sort(function (a, b) {
+  return uniques.sort((a, b) => {
     return frequency[b] - frequency[a];
   });
 }
 
 export function getUserProposals(state) {
   const userId = 2; // for now
-  return state.proposals.filter(
-    (proposal) => proposal.user_id === userId
-  );
+  return state.proposals.filter((proposal) => proposal.user_id === userId);
 }
 
 export function getTradesProposedToUser(state) {
   const userId = 2; // for now
-  const userListings = state.listings.filter(
-    (listing) => listing.user.id === userId
+  const userListingIds = state.listings
+    .filter((listing) => listing.user.id === userId)
+    .map((listing) => listing.id);
+  return state.proposals.filter((proposal) =>
+    userListingIds.includes(proposal.asset_id)
   );
-  const userListingIds = userListings.map((listing) => listing.id);
-  return state.proposals.filter((proposal) => userListingIds.includes(proposal.asset_id));
 }
 
 export function getPicturesOfListingsByUser(state, userProposals) {
-  const offeredItemIds = userProposals.map(
-    (proposal) => proposal.listing_id
-  );
+  const offeredItemIds = userProposals.map((proposal) => proposal.listing_id);
   let allOfferedListings = [];
-    let offeredItem;
-    for (const id of offeredItemIds) {
-      offeredItem = state.listings.find((listing) => listing.id === id);
-      allOfferedListings.push(offeredItem);
-    };
-  return allOfferedListings.map(
-    (offeredItem) => offeredItem.picture
-  );
+  let offeredItem;
+  for (const id of offeredItemIds) {
+    offeredItem = state.listings.find((listing) => listing.id === id);
+    allOfferedListings.push(offeredItem);
+  }
+  return allOfferedListings.map((offeredItem) => offeredItem.picture);
 }
 
 export function getPicturesOfListingsWantedByUser(state, userProposals) {
-  const wantedItemIds = userProposals.map(
-    (proposal) => proposal.asset_id
-  );
+  const wantedItemIds = userProposals.map((proposal) => proposal.asset_id);
   let allWantedListings = [];
-    let wantedItem;
-    for (const id of wantedItemIds) {
-      wantedItem = state.listings.find((listing) => listing.id === id);
-      allWantedListings.push(wantedItem);
-    };
-  return allWantedListings.map(
-    (wantedItem) => wantedItem.picture
-  );
+  let wantedItem;
+  for (const id of wantedItemIds) {
+    wantedItem = state.listings.find((listing) => listing.id === id);
+    allWantedListings.push(wantedItem);
+  }
+  return allWantedListings.map((wantedItem) => wantedItem.picture);
 }
 
 export function getPicturesOfListingsOfferedToUser(state, tradesProposedToUser) {
@@ -62,14 +53,12 @@ export function getPicturesOfListingsOfferedToUser(state, tradesProposedToUser) 
     (proposal) => proposal.listing_id
   );
   let allListingsOfferedToMe = [];
-    let offeredItem;
-    for (const id of offeredToMeIds) {
-      offeredItem = state.listings.find((listing) => listing.id === id);
-      allListingsOfferedToMe.push(offeredItem);
-    }
-  return allListingsOfferedToMe.map(
-    (offeredItem) => offeredItem.picture
-  );
+  let offeredItem;
+  for (const id of offeredToMeIds) {
+    offeredItem = state.listings.find((listing) => listing.id === id);
+    allListingsOfferedToMe.push(offeredItem);
+  }
+  return allListingsOfferedToMe.map((offeredItem) => offeredItem.picture);
 }
 
 export function getPicturesOfListingsWantedFromUser(state, tradesProposedToUser) {
@@ -77,12 +66,10 @@ export function getPicturesOfListingsWantedFromUser(state, tradesProposedToUser)
     (proposal) => proposal.asset_id
   );
   let allListingsTheyWant = [];
-    let wantedItem;
-    for (const id of listingsTheyWantIds) {
-      wantedItem = state.listings.find((listing) => listing.id === id);
-      allListingsTheyWant.push(wantedItem);
-    };
-  return allListingsTheyWant.map(
-    (wantedItem) => wantedItem.picture
-  );
+  let wantedItem;
+  for (const id of listingsTheyWantIds) {
+    wantedItem = state.listings.find((listing) => listing.id === id);
+    allListingsTheyWant.push(wantedItem);
+  }
+  return allListingsTheyWant.map((wantedItem) => wantedItem.picture);
 }
